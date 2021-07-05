f68d26f379ef6eed67de12a9bc7d03ddf2107404
Flavio <flaviomarcosmarinho@gmail.com>
2021-07-04 22:17:52 -0300

    Change MainHeader

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useMemo} from "react";
 
-import { Container } from "./styles";
+import Toggle from '../Toggle'
+import emojis from "../../utils/emojis";
+
+import { 
+    Container, 
+    Profile, 
+    Welcome, 
+    UserName 
+} from "./styles";
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length)
+        return emojis[indice]
+    }, [])
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <Toggle />
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Flávio Marcos Marinho</UserName>
+            </Profile>
         </Container>        
     );
 }
