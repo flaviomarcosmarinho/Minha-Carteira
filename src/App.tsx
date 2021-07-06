f3335c4f612f301ac69a306c510c1d73d760cde7
Flavio <flaviomarcosmarinho@gmail.com>
2021-07-05 22:01:09 -0300

    Create content header

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from 'styled-components';
 import GlobaStyles from "./styles/GlobaStyles";
 
 import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
 import dark from './styles/themes/dark'
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobaStyles />
-            <Layout />
+            <Layout>
+                <Dashboard />
+            </Layout>
         </ThemeProvider>
     );
 }
---
+++ src/components/ContentHeader/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    width: 100%;
+    
+    display: flex;
+    justify-content:  space-between; /*Suficiente para cada um ficar de um lado*/
+
+    margin-bottom: 25px;    
+`;
+
+export const TitleContainer = styled.div `
+    > h1 { /* o sinal de > é para garantir que vou alterar somente o H1 que está no TitleContainer, e não sair alterando todos h1 que tem no script */
+        color: ${props => props.theme.colors.white };
+
+        &::after { /* Pseudo elemento */
+            content: ''; 
+            display: block;
+            width: 55px;
+            border-bottom: 10px solid ${props => props.theme.colors.warning};
+        }
+    }
+`;
+
+export const Controllers = styled.div ``;
