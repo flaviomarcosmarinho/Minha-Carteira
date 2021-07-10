b70618ad44f080169b3f402b98c9d629121050c8
Flavio <flaviomarcosmarinho@gmail.com>
2021-07-10 11:50:51 -0300

    Add list data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,11 @@
-import React, {useMemo} from "react";
+import React, {useMemo, useState, useEffect} from "react";
 
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
+
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 
 import { Container, Content, Filters } from './styles'
 
@@ -14,18 +17,32 @@ interface IRouteParams {
     }
 }
 
-const List: React.FC<IRouteParams> = ({ match }) => {
+//Interface para carregar os dados jSon que serão recuperados da API
+interface IData {
+    id: string;
+    description: string;
+    amountFormatted: string;
+    frequency: string;
+    dataFormatted: string;
+    tagColor: string;
+}
 
+const List: React.FC<IRouteParams> = ({ match }) => {
+    const [data, setData] = useState<IData[]>([]);
     const { type } = match.params;
 
     /* Pegando o parametro passado pela rota para alterar o title */
     /* Para fazer isto, utiliza-se o Hook do React useMemo passando o [type] para ficar monitorando a lateração */
     const title = useMemo(() => {
-        return type === 'entry-balance' ? 'Entradas':'Saídas'        
+        return type === 'entry-balance' ? 'Entradas':'Saídas'
     }, [type]);
 
     const lineColor = useMemo(() => {
         return type === 'entry-balance' ? '#F7931B' : '#E44C4E'
+    }, [type]);
+
+    const listData = useMemo(() => {
+        return type === 'entry-balance' ? gains : expenses;
     }, [type]);
 
     const months = [
@@ -39,6 +56,20 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         {value: 2019, label: 2019},
         {value: 2018, label: 2018},
     ];
+
+    useEffect(() => {
+        const response = listData.map(item => {
+            return {
+                id: String(Math.random() * data.length), //Criando um id aleatório com base no tamanho da lista
+                description: item.description,
+                amountFormatted: item.amount,
+                frequency: item.frequency,
+                dataFormatted: item.date,
+                tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
+            }
+        })
+        setData(response);
+    },[]);
 
     return (
        <Container>
@@ -58,12 +89,17 @@ const List: React.FC<IRouteParams> = ({ match }) => {
             </Filters>
 
             <Content>
-                <HistoryFinanceCard                     
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="06/07/2021"
-                    amount="R$ 130,00"
-                />                
+                {
+                    data.map(item => (
+                        <HistoryFinanceCard
+                            key={item.id}
+                            tagColor={item.tagColor}
+                            title={item.description}
+                            subtitle={item.dataFormatted}
+                            amount={item.amountFormatted}
+                        /> 
+                    ))
+                }               
             </Content>
        </Container>
     );
